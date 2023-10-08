69c305193a583af0afa956c4936b6ee567a820db
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-10-08 15:00:37 -0300

    Ajusta WebConfig

--- src/main/java/com/r2da/desafioapi/config/WebConfig.java
+++ src/main/java/com/r2da/desafioapi/config/WebConfig.java
@@ -1,12 +1,14 @@
 package com.r2da.desafioapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry){
